ed31969752d378d0280d9ef7d8eb30be777b76a7
jinsub-kim-dev <jinsub.kim.dev@gmail.com>
2020-08-17 17:54:47 +0900

    Main application에 base package 경로 설정

--- src/main/java/com/jinsub/housekeeping/HousekeepingBookApplication.java
+++ src/main/java/com/jinsub/housekeeping/HousekeepingBookApplication.java
@@ -2,10 +2,12 @@ package com.jinsub.housekeeping;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@ComponentScan(basePackages = "com.jinsub.housekeeping")
 public class HousekeepingBookApplication {
 
 	public static void main(String[] args) {
